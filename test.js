201d6ab2ef27b9610b60d1b354e932977ebaff69
lanns-skynns <56886235+lanns-skynns@users.noreply.github.com>
2020-10-06 21:19:48 +0200

    v 1.3

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 
-function hr(){
+function hr(etatDisplay){
     var hr=document.querySelectorAll('hr');
     for( var i=0; i<hr.length;++i){
-        hr[i].style=" display : none;"
+        hr[i].style.display=etatDisplay;
     }
 }
 
 function connexion(balise,i){
     balise.addEventListener("click",function(){
-        //var AffichageInput= document.querySelectorAll(".Input")[i]; 
+        //var AffichageInput= document.querySelectorAll(".Input")[i];
         var AffichageInput= document.querySelectorAll("p[class~='Input']")[i];
         AffichageInput.style="display : inline;"
 
@@ -18,23 +18,32 @@ function connexion(balise,i){
                 div[k].style="display : none;"
             }
         }
-      hr();
-    });
-}
+      hr("none");
+      var retour=document.getElementById("retour");
+      retour.style.display="block";
 
+});
+}
 
 var typeUser=document.querySelectorAll("h2");
 
 connexion(typeUser[0],0);
 connexion(typeUser[1],1);
-connexion(typeUser[2],2)
+connexion(typeUser[2],2);
 
-
-
-
-
-
-
+var retour=document.getElementById("retour")
+retour.addEventListener("click",function(){
+    var div=document.querySelectorAll("div");
+    for (var k=0; k<div.length;++k){
+            div[k].style="display : block;"
+    }
+    var affichageInput=document.getElementsByClassName("Input");
+    for (var j=0; j<affichageInput.length;++j){
+        affichageInput[j].style="display : none;"
+}
+    hr("block");
+    retour.style.display="none"
+})
 
 
 
