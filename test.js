00df7b27b25045ed82a6d2bebe225fd499728218
lanns-skynns <56886235+lanns-skynns@users.noreply.github.com>
2020-10-06 09:48:37 +0200

    v 1.2

--- test.js
+++ test.js
@@ -43,21 +43,21 @@ function hr(){
 
 
 
-function connexion(i){
-    alert(i)
-    var AffichageInput= document.querySelectorAll(".Input")[i];
-    AffichageInput.style="display : block;"
+function connexion(balise,i){
+    balise.addEventListener("click",function(){
+        var AffichageInput= document.querySelectorAll(".Input")[i];
+        AffichageInput.style="display : block;"
 
-    var div=document.querySelectorAll("div");
-    for (var k=0; k<div.length;++k){
-        if( k!=i){
-            div[k].style="display : none;"
+        var div=document.querySelectorAll("div");
+        for (var k=0; k<div.length;++k){
+            if( k!=i){
+                div[k].style="display : none;"
+            }
         }
-    }
-    hr();
+      hr();
+
+    });
 }
-
-
 
 
 
@@ -65,11 +65,12 @@ var typeUser=document.querySelectorAll("p");
 
 
 
-for (var j=0; j<typeUser.length;++j){
-    typeUser[j].addEventListener("mouseover",function(){
-         connexion(1);
-});
-}
+
+connexion(typeUser[0],0);
+connexion(typeUser[1],1);
+connexion(typeUser[2],2)
+
+
 
 var div=document.querySelectorAll("div");
 
