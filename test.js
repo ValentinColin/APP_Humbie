fc11a769e062414e940e1c05575f7d1f6f343753
lanns-skynns <56886235+lanns-skynns@users.noreply.github.com>
2020-10-09 11:05:13 +0200

    new

--- test.js
+++ test.js
@@ -1,8 +1,7 @@
-
-function hr(etatDisplay){
+function hr(){
     var hr=document.querySelectorAll('hr');
     for( var i=0; i<hr.length;++i){
-        hr[i].style.display=etatDisplay;
+        hr[i].style=" display : none;"
     }
 }
 
@@ -18,35 +17,13 @@ function connexion(balise,i){
                 div[k].style="display : none;"
             }
         }
-      hr("none");
-      var retour=document.getElementById("retour");
-      retour.style.display="block";
-
-});
+      hr();
+    });
 }
+
 
 var typeUser=document.querySelectorAll("h2");
 
 connexion(typeUser[0],0);
 connexion(typeUser[1],1);
-connexion(typeUser[2],2);
-
-var retour=document.getElementById("retour")
-retour.addEventListener("click",function(){
-    var div=document.querySelectorAll("div");
-    for (var k=0; k<div.length;++k){
-            div[k].style="display : block;"
-    }
-    var affichageInput=document.getElementsByClassName("Input");
-    for (var j=0; j<affichageInput.length;++j){
-        affichageInput[j].style="display : none;"
-}
-    hr("block");
-    retour.style.display="none"
-})
-
-
-
-
-
-
+connexion(typeUser[2],2)
