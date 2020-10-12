3a1bf9c440e0aff15307a3764b1a5b121f0ac1b6
Valentin Colin <valentin.colin78@gmail.com>
2020-10-12 09:18:43 +0200

    many changes

--- test.js
+++ test.js
@@ -1,29 +1,29 @@
 
-function hr(){
+function hr_clear(){
     var hr=document.querySelectorAll('hr');
-    for( var i=0; i<hr.length;++i){
-        hr[i].style=" display : none;"
+    for( var i=0; i<hr.length; ++i){
+        hr[i].style = " display : none;"
     }
 }
 
 function connexion(balise,i){
     balise.addEventListener("click",function(){
         //var AffichageInput= document.querySelectorAll(".Input")[i]; 
-        var AffichageInput= document.querySelectorAll("p[class~='Input']")[i];
-        AffichageInput.style="display : inline;"
+        var AffichageInput = document.querySelectorAll("p[class~='Input']")[i];
+        AffichageInput.style = "display : inline;"
 
         var div=document.querySelectorAll("div");
-        for (var k=0; k<div.length;++k){
+        for (var k=0; k<div.length; ++k){
             if( k!=i){
-                div[k].style="display : none;"
+                div[k].style = "display : none;"
             }
         }
-      hr();
+      hr_clear();
     });
 }
 
 
-var typeUser=document.querySelectorAll("h2");
+var typeUser = document.querySelectorAll("h2");
 
 connexion(typeUser[0],0);
 connexion(typeUser[1],1);
