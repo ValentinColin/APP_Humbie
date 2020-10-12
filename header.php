3a6bc155a14ebc84ba89a2a0da9cc1c6116a186e
Valentin Colin <valentin.colin78@gmail.com>
2020-10-12 11:24:11 +0200

    organisation

---
+++ header.php
@@ -0,0 +1,13 @@
+<header>
+	<h1>Nom de l'application</h1>
+
+	<div class="Research">
+		<input type="text" name="Research" placeholder="Research" required >
+		<span><a id="RechercheAvancer" href="">Recherche avancer</a></span>
+	</div>
+
+	<div class="Profil">
+		<figure title="Profil"><img id="Photo" src="lien_photo.png" onclick="lien_vers_la_page_de_profil.html"></figure>
+		<span><a id="profilName" href="">PRÉNOM NOM</a></span>
+	</div>
+</header>
