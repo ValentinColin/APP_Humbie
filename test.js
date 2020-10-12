74ffaf996ce6f19c4a72708640134c4ed3549f31
Valentin Colin <valentin.colin78@gmail.com>
2020-10-12 11:45:48 +0200

    reset

---
+++ test.js
@@ -0,0 +1,67 @@
+var valider = document.querySelector(".valider");
+
+
+function hr(etatDisplay){
+    var hr=document.querySelectorAll('hr');
+    for( var i=0; i<hr.length;++i){
+        hr[i].style.display=etatDisplay;
+    }
+}
+
+function connexion(balise,i){
+    balise.addEventListener("click",function(){
+        //var AffichageInput= document.querySelectorAll(".Input")[i];
+        var AffichageInput= document.querySelectorAll("p[class~='Input']")[i];
+        AffichageInput.style="display : inline;"
+
+        var div=document.querySelectorAll("div");
+        for (var k=0; k<div.length;++k){
+            if( k!=i){
+                div[k].style="display : none;"
+            }
+        }
+      hr("none");;
+      retour.style.display="block";
+      valider.style.display="block";
+
+
+    });
+}
+
+var typeUser=document.querySelectorAll("h2");
+
+connexion(typeUser[0],0);
+connexion(typeUser[1],1);
+connexion(typeUser[2],2)
+
+var retour=document.getElementById("retour")
+retour.addEventListener("click",function(){
+    var div=document.querySelectorAll("div");
+    for (var k=0; k<div.length;++k){
+            div[k].style="display : block;"
+    }
+    var affichageInput=document.getElementsByClassName("Input");
+    for (var j=0; j<affichageInput.length;++j){
+        affichageInput[j].style="display : none;"
+
+}
+    hr("block");
+    retour.style.display="none"
+
+      valider.style.display="none";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
