07e08312dfc6bd6392cabf7b367a8b6ee12e65c7
Valentin Colin <valentin.colin78@gmail.com>
2020-10-16 00:35:52 +0200

    automatisation: id_manager

--- php/create_user.php
+++ php/create_user.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 try
 {
 	$bdd = new PDO('mysql:host=localhost;dbname=Humbie;charset=utf8','root','root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
@@ -7,10 +8,18 @@ catch(Exception $e)
 {
         die('Erreur : '.$e->getMessage());
 }
- 
+
+$req = $bdd->query('SELECT id
+                    FROM Members 
+                    WHERE pseudo = \'' . $_SESSION['pseudo'] . '\''
+                    );
+
+$id_manager = $req->fetch()['id'];
+$req->closeCursor();
+
 $bdd->exec('
-	INSERT INTO Members (email, id_manager, prenom, nom, birthday_date, aviation_licence_date) 
-	VALUES ' . $_POST['prenom'] . $_POST['nom'] . $_POST['email'] . $_POST['birthday_date'] . $_POST['aviation_licence_date'] . $_POST['id_manager'] 
+	INSERT INTO Members (id_manager, prenom, nom, email, birthday_date, aviation_licence_date) 
+	VALUES ' . $id_manager . $_POST['prenom'] . $_POST['nom'] . $_POST['email'] . $_POST['birthday_date'] . $_POST['aviation_licence_date']
 	);
 
 
