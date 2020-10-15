964874745bb7c26482303c32490ac8a1e30077fe
Valentin Colin <valentin.colin78@gmail.com>
2020-10-15 02:09:14 +0200

    ajout d'un menu basique (lien inactif)

--- php/main.php
+++ php/main.php
@@ -10,22 +10,32 @@ if(!$_SESSION['connected']){
 <head>
 	<meta charset="UTF-8">
 	<title>Humbie</title>
+	<!-- <link rel="stylesheet" type="text/css" href="../css/header.css"> -->
+	<link rel="stylesheet" type="text/css" href="../css/main.css">
+	<!-- <link rel="stylesheet" type="text/css" href="../css/footer.css"> -->
 </head>
 <header>
     <!-- En-tête -->
-</header>
-<nav>
-	<!-- Navigation (ie. Menu) -->
-</nav>
-<body>
-	<!-- Corps -->
-	<?php 
+    <?php 
 	echo '<p>Bonjour ' . $_SESSION['prenom'] . ' ' . $_SESSION['nom'] . ' !</p>';
-	echo '<p>Bienvenue sur le site web de Humbie ©. Vous êtes actuellement sur la page principale.</p>';
+	echo '<p>Bienvenue sur le site web de Humbie ©.<br>Vous êtes actuellement sur la page principale.</p>';
 	?>
     <form method='post' action='logout.php'>
         <input type='submit' value='déconnexion'>
     </form>
+</header>
+<nav>
+	<!-- Navigation (ie. Menu) -->
+	<ul id="navigation">
+		<li><a href="#" title="aller à la section 1 (en cours de construction)">section 1 (en cours de construction)</a></li>
+		<li><a href="#" title="aller à la section 2 (en cours de construction)">section 2 (en cours de construction)</a></li>
+		<li><a href="#" title="aller à la section 3 (en cours de construction)">section 3 (en cours de construction)</a></li>
+		<li><a href="#" title="aller à la section 4 (en cours de construction)">section 4 (en cours de construction)</a></li>
+		<li><a href="#" title="aller à la section 5 (en cours de construction)">section 5 (en cours de construction)</a></li>
+	</ul>
+</nav>
+<body>
+	<!-- Corps -->
 </body>
 <footer>
 	<!-- Pied de page -->
