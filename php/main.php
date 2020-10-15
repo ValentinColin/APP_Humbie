e26c339fcde11b51fb38a91d3a52d89e8c3f643c
Crakleurs <eti.faviere@gmail.com>
2020-10-15 20:32:21 +0200

    Ajout navbar

--- php/main.php
+++ php/main.php
@@ -15,17 +15,28 @@ if(!$_SESSION['connected']){
 	<!-- <link rel="stylesheet" type="text/css" href="../css/footer.css"> -->
 </head>
 <header>
-    <!-- En-tête -->
-    <?php 
-	echo '<p>Bonjour ' . $_SESSION['prenom'] . ' ' . $_SESSION['nom'] . ' !</p>';
-	echo '<p>Bienvenue sur le site web de Humbie ©.<br>Vous êtes actuellement sur la page principale.</p>';
-	?>
-    <form method='post' action='logout.php'>
-        <input type='submit' value='déconnexion'>
-    </form>
+	<!-- En-tête -->
+
 </header>
 <nav>
 	<!-- Navigation (ie. Menu) -->
+	<nav>
+		<ul id="navbar">
+			<li><a href="#" title="navbar_HOME">Home</a></li>
+			<li><a href="#" title="navbar_PROFIL">Profil</a></li>
+			<li><a href="#" title="navbar_MENU">Menu</a></li>
+			<li><a href="#" title="navbar_PARAMETRE">Paramètres</a></li>
+			
+			<li><div class="burger">
+				<div class="line1"></div>
+				<div class="line2"></div>
+				<div class="line3"></div>
+			</div></li>
+		
+		</ul>
+
+	</nav>
+
 	<ul id="navigation">
 		<li><a href="#" title="aller à la section 1 (en cours de construction)">section 1 (en cours de construction)</a></li>
 		<li><a href="#" title="aller à la section 2 (en cours de construction)">section 2 (en cours de construction)</a></li>
@@ -33,6 +44,15 @@ if(!$_SESSION['connected']){
 		<li><a href="#" title="aller à la section 4 (en cours de construction)">section 4 (en cours de construction)</a></li>
 		<li><a href="#" title="aller à la section 5 (en cours de construction)">section 5 (en cours de construction)</a></li>
 	</ul>
+
+	<?php 
+	
+	echo '<p>Bonjour ' . $_SESSION['prenom'] . ' ' . $_SESSION['nom'] . ' !</p>';
+	echo '<p>Bienvenue sur le site web de Humbie ©.<br>Vous êtes actuellement sur la page principale.</p>';
+	?>
+    <form method='post' action='logout.php'>
+        <input type='submit' value='déconnexion'>
+    </form>
 </nav>
 <body>
 	<!-- Corps -->
