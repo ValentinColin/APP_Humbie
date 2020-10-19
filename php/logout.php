e4dc15f46d4e919ab844c91dfd0b18d37a3efba7
Valentin Colin <valentin.colin78@gmail.com>
2020-10-19 08:46:47 +0200

    building page, lien temporaire, logo onglet, etc

--- php/logout.php
+++ php/logout.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
-$_SESSION['connected']=0;
-header('Location: maquette.php');
 
+// Suppression des variables de session et de la session
+$_SESSION = array();
+session_destroy();
+
+// Redirection
+header('Location: maquette.php');
 ?>
--- php/main.php
+++ php/main.php
@@ -18,40 +18,53 @@ function generate_path_photo(){
 	<link rel="stylesheet" type="text/css" href="../css/header.css">
 	<link rel="stylesheet" type="text/css" href="../css/main.css">
 	<link rel="stylesheet" type="text/css" href="../css/footer.css">
+  	<link rel="icon" href="../Images/logo_Humbie.png">
 </head>
 <body>
 <header>
 	<!-- En-tête -->
 	<ul id="header_nav">
-		<li><a href="#" title="Menu"><img id="icon_burger_menu" src="../Images/icon-burger-menu.png"></a></li>
-		<li><a href="#" title="header_nav_HOME">Home (icone)</a></li>
+		<li><a href="../html/building.html" title="Menu"><img id="icon_burger_menu" src="../Images/icon-burger-menu.png"></a></li>
+		<li><a href="../html/building.html" title="header_nav_HOME">Home (icone)</a></li>
 		<li><form method="post" action="">
 				<input type="text" name="Research" placeholder="Research">
 				<span><a id="RechercheAvancer" href="">Recherche avancer</a></span>
 				<input type="submit" name="Searching button" value="Search">
 			</form></li>
-		<li><a href="#" title="header_nav_PARAMETRE">Paramètres (icone)</a></li>
-		<li><img id="photo_profil" src=<?php echo generate_path_photo() ?>><a href="#" title="header_nav_PROFIL"><?php echo $_SESSION['prenom'].' '.$_SESSION['nom'] ?></a></li>
+		<li><a href="../html/building.html" title="header_nav_PARAMETRE">Paramètres (icone)</a></li>
+		<li><img id="photo_profil" src=<?php echo generate_path_photo() ?>>
+			<a href="../html/building.html" title="header_nav_PROFIL"><?php echo $_SESSION['prenom'].' '.$_SESSION['nom'] ?></a></li>
 		<li><form method="post" action="logout.php"><input type="submit" value="déconnexion"></form></li>
 	</ul>
 </header>
 <nav>
 	<!-- Navigation (ie. Menu) -->
 	<ul id="navigation">
-		<li><a href="#" title="aller à la section 1 (en cours de construction)">section 1 (en cours de construction)</a></li>
-		<li><a href="#" title="aller à la section 2 (en cours de construction)">section 2 (en cours de construction)</a></li>
-		<li><a href="#" title="aller à la section 3 (en cours de construction)">section 3 (en cours de construction)</a></li>
-		<li><a href="#" title="aller à la section 4 (en cours de construction)">section 4 (en cours de construction)</a></li>
-		<li><a href="#" title="aller à la section 5 (en cours de construction)">section 5 (en cours de construction)</a></li>
+		<li><a id="menu1" href="../html/building.html" title="section 1 (building...)">section 1 (building...)</a></li>
+		<li><a id="menu2" href="../html/building.html" title="section 2 (building...)">section 2 (building...)</a></li>
+		<li><a id="menu3" href="../html/building.html" title="section 3 (building...)">section 3 (building...)</a></li>
+		<li><a id="menu4" href="../html/building.html" title="section 4 (building...)">section 4 (building...)</a></li>
+		<li><a id="menu5" href="../html/building.html" title="section 5 (building...)">section 5 (building...)</a></li>
 	</ul>
 </nav>
-<main>
+<main onload="draw();">
 	<!-- Corps -->
 	<hr>
 	<?php 
 	echo '<p>Bonjour ' . $_SESSION['prenom'] . ' ' . $_SESSION['nom'] . ' !</p>';
 	echo '<p>Vous êtes actuellement sur la page principale.</p>';
 	?>
+	<?php 
+	for ($i=10; $i < 10; $i++) { 
+	 	echo 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+	 } ?>
+	 <canvas id="test" height="300" width="300"></canvas>
+	 
 	<hr>
 
 </main>
@@ -59,13 +72,16 @@ function generate_path_photo(){
 	<!-- Pied de page -->
 	<p>Copyright © 2020 - Humbie Corp. All rights reserved.</p>
 	<ul id="footer_nav">
-		<li><a href="#" title="Politique de confidentialité">Politique de confidentialité</a></li>
-		<li><a href="#" title="I do something">something</a></li>
-		<li><a href="#" title="FAQ">FAQ</a></li>
+		<li><a href="../html/building.html" title="Politique de confidentialité">Politique de confidentialité</a></li>
+		<li><a href="../html/building.html" title="I do something">something</a></li>
+		<li><a href="../html/building.html" title="FAQ">FAQ</a></li>
 		<li><a href="mailto:valentin.colin78@gmail.com" title="valentin.colin78@gmail.com">Nous contacter</a></li>
-		<li><a id="lang_fr" href="../fr/main.php">Français</a>/<a id="lang_en" href="../fr/main.php">English</a></li>
+		<li><a>Français</a>/<a id="lang_en" href="../html/building.html">English</a></li>
 	</ul>
 </footer>
 
 </body>
+
+<script src="../js/main.js"></script>
+
 </html>
