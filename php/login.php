5bf746171a70d1538c5b73d6495bc1fbe2141104
Valentin <valentin.colin78@gmail.com>
2020-10-20 18:22:29 +0200

    Simplification du code (et correction d'une erreur)
    php renvoie une erreurs lorsqu'il ne trouvait pas de personne dans la bdd et que l'on essayait quand même de tester la valeur du mot de passe

--- php/login.php
+++ php/login.php
@@ -16,40 +16,28 @@ if(!empty($_POST['pseudo']) and !empty($_POST['password']))
     // Indique qu'on a fini de traité la requête (permet d'éviter des problèmes lors de futurs requête)
     $req->closeCursor();
 
-    // Fonction inutilisable tant que le mot de passe n'a pas été hasher
-    // Comparaison du pass envoyé via le formulaire avec la base
-    //$isPasswordCorrect = password_verify($_POST['password'], $data['password']);
 
-    // En attendant que la fonction password_verify() puisse être utiliser...
-    $isPasswordCorrect = false;
-    if ($_POST['password'] === $data['password']) 
-    { 
-        $isPasswordCorrect = true; 
-    }
-
-
+    // Si on a trouver aucune personne dans la base de données (ie. pseudo incorrect)
     if (empty($data))
     {
         $erreur = 'Mauvais mot de passe ou pseudo !';
-
     }
+    // Sinon si le mdp est correct
+    // else if (password_verify($_POST['password'], $data['password']))
+    else if ($_POST['password'] === $data['password']) 
+    {
+        $_SESSION['id'] = $data['id'];
+        $_SESSION['pseudo'] = $data['pseudo'];
+        $_SESSION['prenom'] = $data['prenom'];
+        $_SESSION['nom'] = $data['nom'];
+        $_SESSION['connected'] = true;
+        echo 'Vous êtes connecté !';
+        
+        // redirection
+        header('Location: main.php');
+    } 
     else
     {
-        if ($isPasswordCorrect) 
-        {
-            $_SESSION['id'] = $data['id'];
-            $_SESSION['pseudo'] = $data['pseudo'];
-            $_SESSION['prenom'] = $data['prenom'];
-            $_SESSION['nom'] = $data['nom'];
-            $_SESSION['connected'] = true;
-            echo 'Vous êtes connecté !';
-            
-            // redirection
-            header('Location: main.php');
-        }
-        else 
-        {
-            $erreur = 'Mauvais mot de passe ou pseudo !';
-        }
+        $erreur = 'Mauvais mot de passe ou pseudo !';
     }
 }
