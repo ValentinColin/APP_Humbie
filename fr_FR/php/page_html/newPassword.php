ccad50bcbc16144dd4ca7df4ba616a4f23827764
Valentin <valentin.colin78@gmail.com>
2020-11-07 23:26:07 +0100

    REORGANISATION DES FICHIERS POUR LA LANGUE

---
+++ fr_FR/php/page_html/newPassword.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+if ($_SESSION['connected']){
+    header('Location: home.php');
+}
+$erreur = null;
+require_once('../php_pur/login.php');
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title> Réinitialisation de mot de passe </title>
+    <link rel="stylesheet" href="../../../css/loginPage.css">
+    <link rel="stylesheet" href="../../../css/newPassword.css">
+
+</head>
+
+<body>
+    <center>
+    <header>
+        <h1>Page Réinitialisation</h1>
+        <!-- <img id="compagnyLogo"src="../Images/Infini_Measures.png" alt=""> -->
+    </header>
+
+    <main>
+        <form method="post" action="password.php">
+            <fieldset >
+                <legend>Réinitialisation de mot de passe</legend>
+
+                <a class='backLoginPage' href='loginPage.php' title="Retouner à la page de connexion"> <<< </a>
+                <p class="input">
+                <span id="errorDisplay"> Adresse mail incorrecte </span>
+                    <br id='br'>
+                    Adresse mail  :  <input id="mail" type="mail" name="mail"  placeholder="ceci-est-n@exemple.fr" required><br>
+                </p>
+                <hr>
+                <input id='submit' type='submit' value='Réinitialiser'>
+            </fieldset>
+        </form>
+    </main>
+    </center>
+
+
+</body>
+<script src="../../js/newPassword.js"></script>
+
+</html>
---
+++ fr_FR/php/page_html/profil.php
@@ -0,0 +1,89 @@
+<?php 
+session_start();
+include('../php_pur/function.php');
+
+// On vérifie toujours si le visiteur est connecté, sinon on le redirige vers la page demander
+if_not_connected($redirection='loginPage.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profil</title>
+	<link rel="stylesheet" type="text/css" href="../../../css/config.css">
+	<!-- <link rel="stylesheet" type="text/css" href="../../../css/default.css"> -->
+	<link rel="stylesheet" type="text/css" href="../../../css/header.css">
+	<link rel="stylesheet" type="text/css" href="../../../css/profil.css">
+	<link rel="stylesheet" type="text/css" href="../../../css/footer.css">
+</head>
+<body>
+	<?php require('header.php'); ?>
+
+	<div class="wrapper">
+		<div id="box-title" class="my-block">
+			<div><a href="../../../html/building.html" title="Menu"><img class="icon" src="../../../Images/icon-burger-menu.png"></a></div>
+			<h1> > Profil</h1>
+		</div>
+		<div id="box-nav" class="my-block">
+			<h2>Détails professionnel</h2>
+			<img id="photo_profil_big" src=<?php echo path_photo() ?>>
+			<table>
+				<tr> <!-- ligne 1 -->
+					<td><strong>NOM</strong></td> <!-- colonne 1 -->
+					<td><?php echo $_SESSION['prenom'] .' '. $_SESSION['nom'] ?></td> <!-- colonne 2 -->
+				</tr>
+				<tr> <!-- ligne 2 -->
+					<td><strong>AGE</strong></td> <!-- colonne 1 -->
+					<td>UNKNOW</td> <!-- colonne 2 -->
+				</tr>
+				<tr> <!-- ligne 3 -->
+					<td><strong>RÉSIDENCE</strong></td> <!-- colonne 1 -->
+					<td>UNKNOW</td> <!-- colonne 2 -->
+				</tr>
+				<tr> <!-- ligne 4 -->
+					<td><strong>EXPÉRIENCE</strong></td> <!-- colonne 1 -->
+					<td>UNKNOW</td> <!-- colonne 2 -->
+				</tr>
+				<tr> <!-- ligne 5 -->
+					<td><strong>E-MAIL</strong></td> <!-- colonne 1 -->
+					<td><?php echo $_SESSION['mail'] ?></td> <!-- colonne 2 -->
+				</tr>
+			</table>
+		</div>
+		<div id="box-content" class="my-block">
+			<button class="pull-right">Modifier mon profil</button> <!-- Ce bouton doit être placer avant le h2 à cause du float (à modifier plus tard) -->
+			<h2>A propos de moi</h2>
+
+			<hr>
+			<table>
+				<tr> <!-- ligne 1 -->
+					<th class="strong-cell"><strong>NOM</strong></td> <!-- colonne 1 -->
+					<td class="unstrong-cell"><?php echo $_SESSION['prenom'] .' '. $_SESSION['nom'] ?></td> <!-- colonne 2 -->
+					<td class="strong-cell"><strong>AGE</strong></td> <!-- colonne 3 -->
+					<td class="unstrong-cell">UNKNOW</td> <!-- colonne 4 -->
+				</tr>
+				<tr> <!-- ligne 2 -->
+					<th class="strong-cell"><strong>RÉSIDENCE</strong></td> <!-- colonne 1 -->
+					<td class="unstrong-cell">UNKNOW</td> <!-- colonne 2 -->
+					<td class="strong-cell"><strong>EXPÉRIENCE</strong></td> <!-- colonne 3 -->
+					<td class="unstrong-cell">UNKNOW</td> <!-- colonne 4 -->
+				</tr>
+				<tr> <!-- ligne 3 -->
+					<th class="strong-cell"><strong>TELEPHONE</strong></td> <!-- colonne 1 -->
+					<td class="unstrong-cell">UNKNOW</td> <!-- colonne 2 -->
+					<td class="strong-cell"><strong>E-MAIL</strong></td> <!-- colonne 3 -->
+					<td class="unstrong-cell"><?php echo $_SESSION['mail'] ?></td> <!-- colonne 4 -->
+				</tr>
+			</table>
+
+			<br>
+
+			<h2>Test</h2>
+			<hr>
+			<p>AFFICHER LES RESULTATS DES TESTS</p>
+		</div>
+	</div>
+	
+	<?php require('footer.php'); ?>
+</body>
+</html>
