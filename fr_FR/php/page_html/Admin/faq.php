111e3a4094a27324b8a07c32c5234dd48e5f2788
Crakleurs <eti.faviere@gmail.com>
2020-11-20 23:54:41 +0100

    page create user pour admin
    ...........................................................................

--- fr_FR/php/page_html/Admin/faq.php
+++ fr_FR/php/page_html/Admin/faq.php
@@ -13,7 +13,6 @@ $reponse = $bdd->query('
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
 </head>
 <body>
 <?php
