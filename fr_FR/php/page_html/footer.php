0813f724b9fd47d4c644ba7b6fc5914032022140
Crakleurs <eti.faviere@gmail.com>
2020-11-20 18:06:05 +0100

    erreur

--- fr_FR/php/page_html/footer.php
+++ fr_FR/php/page_html/footer.php
@@ -4,7 +4,7 @@
 	<ul id="footer_nav">
 		<li><a href="../../../html/building.html" title="Politique de confidentialité">Politique de confidentialité</a></li>
 		<li><a href="../../../html/building.html" title="I do something">something</a></li>
-		<li><a href="faq.php" title="FAQ">FAQ</a></li>
+		<li><a href="User/faq.php" title="FAQ">FAQ</a></li>
 		<li><a href="mailto:service.humbie@gmail.com" title="service.humbie@gmail.com">Nous contacter</a></li>
 		<!-- Modifier la ligne suivante avec du php -->
 		<li><a>Français</a>/<a id="lang_en" href="../../../html/building.html">English</a></li>
