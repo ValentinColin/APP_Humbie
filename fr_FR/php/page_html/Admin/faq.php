7f86f0b181852a7a6d9d04944eeed5ea209bd331
Crakleurs <eti.faviere@gmail.com>
2020-11-20 19:54:34 +0100

    FAQ admin et modifications

--- fr_FR/php/page_html/Admin/faq.php
+++ fr_FR/php/page_html/Admin/faq.php
@@ -3,18 +3,49 @@
 $bdd = new PDO('mysql:host=localhost;dbname=Humbie', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
 $reponse = $bdd->query('
-	SELECT question, answer
+	SELECT question, answer,id
 	FROM Faq ');
 
+    ?>
 
-while ($donnees = $reponse->fetch())
-{   
-    echo 'Questions : <br>';
-    echo $donnees['question'].'<br> <br>';
-    echo 'Réponses : <br>';
-    echo $donnees['answer'].'<br> ';
-    echo '<button type="button"> Supprimer </button> <br><br>';
-    
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+    while ($donnees = $reponse->fetch())
+    {   
+        echo 'Questions : <br>';
+        echo $donnees['question'].'<br> <br>';
+        echo 'Réponses : <br>';
+        echo $donnees['answer'];
+        ?>
+        <form method="post" action="faqModifier.php">
+            <input type="submit" value= "Supprimer" name=<?= sprintf('%d', $donnees['id'])?> >
+
+        </form> <br><br>
+
+        <?php
+
+            
+            
+        
+    }
 
 ?>
+
+<form method="post" action="faqModifier.php">
+
+            <input type="text" placeholder="Question" name="Question">
+            <input type="text" placeholder="Réponse" name="Reponse" >
+            <input type="submit" value="Ajouter" name="Ajouter" >
+
+</form>
+
+</body>
+</html>
