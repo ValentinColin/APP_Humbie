eead8e580dc69310200b2b8530f04cda95efc0bf
Crakleurs <eti.faviere@gmail.com>
2020-11-21 20:29:09 +0100

    Affichage lorsqu'il n'y a as de tickets et attentes

--- fr_FR/php/page_html/Admin/ticket.php
+++ fr_FR/php/page_html/Admin/ticket.php
@@ -4,9 +4,8 @@ session_start();
 $bdd = new PDO('mysql:host=localhost;dbname=Humbie', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
 $req =$bdd->query('SELECT  `id_ticket`,`topic`, `subject`, `id_member`, `date_request`, `msg_request`, `status` 
-            FROM `tickets` WHERE `status` = "in_process"'
+                    FROM `tickets` WHERE `status` = "in_process"');
 
-);
 ?>
 
 <!DOCTYPE html>
@@ -16,14 +15,24 @@ $req =$bdd->query('SELECT  `id_ticket`,`topic`, `subject`, `id_member`, `date_re
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-
+    <?php $i=0 ?>
     <?php while($donnees = $req->fetch()):
-        if($donnees['status'] =='in_process'):?>
+        if($i==0){
+            echo '<h1> Les Tickets en attente : </h1> ';
+            $i += 1;
+        }
+        if($donnees['status'] =='in_process'):
+
+            $reponse =$bdd->query('SELECT  `nom`,`prenom`
+                                FROM `members` WHERE `id` = '.$donnees["id_member"]);
+            $datas= $reponse->fetch();
+
+            ?>
 
             <p>
                 topic : <?= $donnees['topic']?> <br>
                 sujet : <?= $donnees['subject']?> <br>
-                Question de : <?= $donnees['id_member']?> <br>
+                Question de : <?= $datas['nom']." ".$datas['prenom']?> <br>
                 réquête émise le : <?= $donnees['date_request'] ?> <br>
                 Contenu: <br> <?= $donnees['msg_request']?>
 
@@ -37,7 +46,11 @@ $req =$bdd->query('SELECT  `id_ticket`,`topic`, `subject`, `id_member`, `date_re
 
 
         <?php endif;
-    endwhile; ?>
+    endwhile; 
+    if($i ==0){
+        echo "<h1> Il n'y a pas de tickets en attente !";
+    }
+    ?>
     
 </body>
 </html>
