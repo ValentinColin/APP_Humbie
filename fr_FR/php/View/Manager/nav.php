326f8d6d0162a48c3c671efb8fa85bb8e1a8beda
Valentin <valentin.colin78@gmail.com>
2020-12-12 14:09:19 +0100

    désactivation d'un lien qui entraine un bug sur le search pour un rôle diff de Admin

--- fr_FR/php/View/Manager/nav.php
+++ fr_FR/php/View/Manager/nav.php
@@ -1,7 +1,7 @@
 <nav id='mainNav'>
 	<ul id="navigation">
-	    <li><a href="../../Controller/search_member_c.php/?search=AllManager">Managers</a></li>
-	    <li><a href="../../Controller/search_member_c.php/?search=AllUser">Mes Pilotes</a></li>
+	    <li><a href="empty">Managers</a></li>
+	    <li><a href="empty">Mes Pilotes</a></li>
 	    <li><a href="empty">Statistiques des Pilotes</a></li>
 	    <li><a href="faq.php">FAQ</a></li>
 	    <li><a href="empty">Support</a></li>
