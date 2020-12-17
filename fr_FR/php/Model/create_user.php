dd5554957af0f992964e9fb8e370f4f81f1af706
Valentin <valentin.colin78@gmail.com>
2020-12-17 19:27:29 +0100

    create_user.php model fonctionnel

--- fr_FR/php/Model/create_user.php
+++ fr_FR/php/Model/create_user.php
@@ -7,7 +7,7 @@ function mail_exist($mail){
 	$req = $bdd->query('SELECT email
                         FROM members
                         WHERE email = \'' . htmlspecialchars($mail) . '\'');
-	$data = $req->fetch(); // Tableau possédant à présent les clés:valeurs de la BDD
+	$data = $req->fetchAll(); // Tableau possédant à présent les clés:valeurs de la BDD
 	$req->closeCursor();
 
 	$exist = (count($data) > 0) ? true : false;
@@ -16,26 +16,23 @@ function mail_exist($mail){
 
 function create_user($data){
 	$bdd = login_bdd();
-	$req = $bdd->prepare('INSERT INTO members (mail,password,access,nom,prenom,birthday_date,aviation_licence_date)
+	$req = $bdd->prepare('INSERT INTO members (email,password,access,nom,prenom,birthday_date,aviation_licence_date)
     					  VALUES (:mail ,:password ,:access ,:nom ,:prenom ,:birthday_date ,:aviation_licence_date)');
 
 	$password_gen = passwordgen();
     $password_gen_hash = passwordhash($password_gen);
 
+    if (empty($data['id_manager']) 			$data['id_manager'] = null; 
+    if (empty($data['license_aviation'])) 	$data['license_aviation'] = null;
+
     $req->execute(array(':mail' => htmlspecialchars($data['mail']),
 					    ':password' => $password_gen_hash,
 					    ':access' => htmlspecialchars($data['role']),
-					    ':nom' => htmlspecialchars($data['nom']),
+					    ':id_man' => htmlspecialchars($data['id_manager']),
+					    ':nom' 	  => htmlspecialchars($data['nom']),
 					    ':prenom' => htmlspecialchars($data['prenom']),
 					    ':birthday_date' => htmlspecialchars($data['birthday']),
-					    ':aviation_licence_date' => htmlspecialchars($data['license_aviation'])
-						));
-    if ($data['role'] == 'USER') {
-    	$req = $bdd->prepare('	UPDATE `members` 
-    							SET `id_manager` = :id_man
-    							WHERE `members`.`mail` = :mail');
-    	$req->execute(array(':mail' => $data['mail'],
-    						':id_man' => htmlspecialchars($data['id_manager'])));
-    }
+					    ':aviation_licence_date' => htmlspecialchars($data['license_aviation'])));
+   
     return $passwordgen;
 }
