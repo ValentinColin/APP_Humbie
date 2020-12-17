00834a0e080b9c33bb9d69180a10da12a5bb158f
Crakleurs <68619527+Crakleurs@users.noreply.github.com>
2020-12-17 18:17:23 +0100

    Lien navbar et tickets

--- fr_FR/php/View/Manager/nav.php
+++ fr_FR/php/View/Manager/nav.php
@@ -4,6 +4,6 @@
 		<li><a href="../../Controller/search_member_c.php/?search=AllManager">Managers</a></li>
 		<li><a href="../../Controller/search_member_c.php/?search=AllMember"> Annuaire</a></li>
 	    <li><a href="faq.php">FAQ</a></li>
-	    <li><a href="empty">Support</a></li>
+	    <li><a href="ticket.php">Tickets</a></li>
 	</ul>
 </nav>
