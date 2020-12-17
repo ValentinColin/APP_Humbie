eb90f7614da855ae32e06971abda6ca278232c1e
Crakleurs <68619527+Crakleurs@users.noreply.github.com>
2020-12-17 13:59:45 +0100

    rajout mvc faq pour user et manager

--- fr_FR/php/View/Manager/faq.php
+++ fr_FR/php/View/Manager/faq.php
@@ -1,18 +1,52 @@
-<?php 
+<?php
+session_start();
+include("../../Controller/function.php");
+include("../../Model/faq.php");
 
-$bdd = new PDO('mysql:host=localhost;dbname=Humbie', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-
-$reponse = $bdd->query('
-	SELECT question, answer
-	FROM Faq ');
-
-
-while ($donnees = $reponse->fetch())
-{   
-    echo 'Questions : <br>';
-    echo $donnees['question'].'<br> <br>';
-    echo 'Réponses : <br>';
-    echo $donnees['answer'].'<br> <br><br>';
-}
+if_not_connected($redirection="../../View/login.php");
 
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>FAQ</title>
+	<link rel="stylesheet" type="text/css" href="../../../../css/config.css">
+	<link rel="stylesheet" type="text/css" href="../../../../css/header.css">
+	<link rel="stylesheet" type="text/css" href="../../../../css/footer.css">
+	<link rel="stylesheet" type="text/css" href="../../../../css/home.css">
+	<link rel="stylesheet" type="text/css" href="../../../../css/faq.css">
+	<!-- <link rel="stylesheet" type="text/css" href="../../../../css/faq.css"> -->
+</head>
+<body>
+	<?php require("header.php"); ?>
+
+
+	<main>
+
+	<div id="box-nav" class="my-block">
+			<?php require('nav.php') ?>
+		</div>
+
+	<div class="wrapper">
+		<div id="box-read-FAQ" class="my-block">
+			<?php // Boucle pour afficher les élements: question/réponse
+			$faq = getFAQ();
+			for ($i=0; $i < count($faq); $i++) { ?>
+				<div class='faq_Box_element'>
+					<h2>Question n°<?= $i+1 ?>:</h2>
+					<p class='question'><?= $faq[$i]["question"] ?></p>
+					<p class='answer'><?= $faq[$i]["answer"] ?></p>
+				</div>
+				<hr>
+			<?php } ?>
+		</div>
+	</div>
+
+	</main>
+
+	<?php require("footer.php"); ?>
+</body>
+</html>
