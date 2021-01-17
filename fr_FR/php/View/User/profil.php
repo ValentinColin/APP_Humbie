2ae581e614606fc7c80fb13eba5fe9a5dbd76faa
Crakleurs <eti.faviere@gmail.com>
2021-01-17 21:40:58 +0100

    Profil

--- fr_FR/php/View/User/profil.php
+++ fr_FR/php/View/User/profil.php
@@ -19,95 +19,53 @@ if_not_connected($redirection = '../../View/login.php');
 	<link rel="stylesheet" type="text/css" href="../../../../css/config.css">
 	<!-- <link rel="stylesheet" type="text/css" href="../../../css/default.css"> -->
 	<link rel="stylesheet" type="text/css" href="../../../../css/header.css">
+	<link rel="stylesheet" type="text/css" href="../../../../css/nav.css">
 	<link rel="stylesheet" type="text/css" href="../../../../css/profil.css">
 	<link rel="stylesheet" type="text/css" href="../../../../css/footer.css">
 </head>
 
 <body>
 	<?php require('header.php'); ?>
+	<?php require('nav.php'); ?>
 
+	<img src="../../../../Images/Remplissage_gauche.png" id="remplissage-gauche">
 	<div class="wrapper">
-		<div id="box-title" class="my-block">
-			<div><a href="../../../../html/building.html" title="Menu"><img class="icon" src="../../../../Images/icon-burger-menu.png"></a></div>
-			<h1>> Profil</h1>
-		</div>
 		<div id="box-nav" class="my-block">
-			<h2>Détails professionnel</h2>
 			<img id="photo_profil_big" src=<?= path_photo() ?>>
-			<table>
-				<tr>
-					<!-- ligne 1 -->
-					<td><strong>NOM</strong></td> <!-- colonne 1 -->
-					<td><?= $_SESSION['prenom'] . ' ' . $_SESSION['nom'] ?></td> <!-- colonne 2 -->
-				</tr>
-				<tr>
-					<!-- ligne 2 -->
-					<td><strong>DATE DE NAISSANCE</strong></td> <!-- colonne 1 -->
-					<td><?= $profil['birthday_date'] ?></td> <!-- colonne 2 -->
-				</tr>
-				<tr>
-					<!-- ligne 3 -->
-					<td><strong>PAYS</strong></td> <!-- colonne 1 -->
-					<td><?= $profil['country'] ?></td> <!-- colonne 2 -->
-				</tr>
-				<tr>
-					<!-- ligne 4 -->
-					<td><strong>EXPÉRIENCE</strong></td> <!-- colonne 1 -->
-					<td>UNKNOW</td> <!-- colonne 2 -->
-				</tr>
-				<tr>
-					<!-- ligne 5 -->
-					<td><strong>E-MAIL</strong></td> <!-- colonne 1 -->
-					<td><?= $profil['email'] ?></td> <!-- colonne 2 -->
-				</tr>
-				<tr>
-					<!-- ligne 5 -->
-					<td><strong>TESTS EFFECTUÉ</strong></td> <!-- colonne 1 -->
-					<td><?= "calcul à faire.." ?></td> <!-- colonne 2 -->
-				</tr>
-			</table>
+
 		</div>
 		<div id="box-content" class="my-block">
-			<form action="../../Controller/profilModifier.php" method="POST">
-			<button class="pull-right">Modifier mon profil</button> <!-- Ce bouton doit être placer avant le h2 à cause du float (à modifier plus tard) -->
-			</form>
-			<h2>A propos de moi</h2>
+			<h1>Informations personnelles</h1>
 
-			<hr>
 			<table>
 				<tr>
 					<!-- ligne 1 -->
-					<th class="strong-cell"><strong>NOM</strong></td> <!-- colonne 1 -->
-					<td class="unstrong-cell"><?= $_SESSION['prenom'] . ' ' . $_SESSION['nom'] ?></td> <!-- colonne 2 -->
-					<td class="strong-cell"><strong>DATE DE NAISSANCE</strong></td> <!-- colonne 3 -->
-					<td class="unstrong-cell"><?= $profil['birthday_date'] ?></td> <!-- colonne 4 -->
+					<th class="strong-cell"><strong>NOM:</strong> <?= $_SESSION['prenom'] . ' ' . $_SESSION['nom'] ?></td> <!-- colonne 1 -->
+					<td class="strong-cell"><strong>DATE DE NAISSANCE:</strong> <?= $profil['birthday_date'] ?></td> <!-- colonne 3 -->
+
 					
 				</tr>
 				<tr>
 					<!-- ligne 2 -->
-					<th class="strong-cell"><strong>PAYS</strong></td> <!-- colonne 1 -->
-					<td class="unstrong-cell"><?= $profil['country'] ?></td> <!-- colonne 2 -->
-					<td class="strong-cell"><strong>EXPÉRIENCE</strong></td> <!-- colonne 3 -->
-					<td class="unstrong-cell">UNKNOW</td> <!-- colonne 4 -->
+					<th class="strong-cell"><strong>PAYS:</strong> <?= $profil['country'] ?></td> <!-- colonne 1 -->
+					<td class="strong-cell"><strong>EXPÉRIENCE:</strong> UNKNOW</td> <!-- colonne 3 -->
 				</tr>
 				<tr>
 					<!-- ligne 3 -->
-					<th class="strong-cell"><strong>TELEPHONE</strong></td> <!-- colonne 1 -->
-					<td class="unstrong-cell"><?= $profil['phone'] ?></td> <!-- colonne 2 -->
-					<td class="strong-cell"><strong>E-MAIL</strong></td> <!-- colonne 3 -->
-					<td class="unstrong-cell"><?= $profil['email'] ?></td> <!-- colonne 4 -->
+					<th class="strong-cell"><strong>TELEPHONE:</strong> <?= $profil['phone'] ?></td> <!-- colonne 1 -->
+					<td class="strong-cell"><strong>E-MAIL:</strong> <?= $profil['email'] ?></td> <!-- colonne 3 -->
 				</tr>
 			</table>
 
-			<br>
+			<form action="../../Controller/profilModifier.php" method="POST">
+			<button id="button-profil-modifier" class="pull-right">Modifier les informations personnelles</button> <!-- Ce bouton doit être placer avant le h2 à cause du float (à modifier plus tard) -->
+			</form>
 
-			<h2>Test</h2>
-			<hr>
-			<p>AFFICHER LES RESULTATS DES TESTS</p>
 		</div>
 	</div>
-
-	<?php require('footer.php'); ?>
+	<span id="footer-position">
+		<?php require('footer.php'); ?>
+	</span>
 </body>
 
 </html>
