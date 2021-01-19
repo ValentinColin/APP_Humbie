1d1aec29375e2d37b5a127c3d7eefd6920fdc605
lanns-skynns <56886235+lanns-skynns@users.noreply.github.com>
2021-01-19 10:18:58 +0100

    fin soulignement barre de menu

--- fr_FR/php/View/User/nav.php
+++ fr_FR/php/View/User/nav.php
@@ -1,15 +1,15 @@
 <nav id='mainNav'>
 	<ul id="navigation">
-		<li><a href="empty">(Pilotes ayant le même manager?)</a></li>
+		<li><a style="text-decoration: none;" href="empty">(Pilotes ayant le même manager?)</a></li>
 		<span id="barre">I</span>
-		<li><a href="../../Controller/search_member_c.php/?search=AllMember../../Controller/search_member_c.php/?search=AllMember">Annuaire</a></li>
+		<li><a style="text-decoration: none;" href="../../Controller/search_member_c.php/?search=AllMember">Annuaire</a></li>
 		<span id="barre">I</span>
-		<li><a href="my-results.php">Mes Résultats de Tests</a></li>
+		<li><a style="text-decoration: none;" href="empty">Mes Résultats de Tests</a></li>
 		<span id="barre">I</span>
-		<li><a href="empty">Mes Statistiques</a></li>
+		<li><a style="text-decoration: none;" href="empty">Mes Statistiques</a></li>
 		<span id="barre">I</span>
-		<li><a href="faq.php">FAQ</a></li>
+		<li><a style="text-decoration: none;" href="faq.php">FAQ</a></li>
 		<span id="barre">I</span>
-		<li><a href="ticket.php">Tickets</a></li>
+		<li><a style="text-decoration: none;" href="ticket.php">Tickets</a></li>
 	</ul>
 </nav>
