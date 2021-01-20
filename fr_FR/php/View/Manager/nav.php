753e74f7c91110b7341fa25fc4e908074884123f
Crakleurs <eti.faviere@gmail.com>
2021-01-20 21:49:56 +0100

    ajout resultats flux

--- fr_FR/php/View/Manager/nav.php
+++ fr_FR/php/View/Manager/nav.php
@@ -1,6 +1,8 @@
 <nav id='mainNav'>
 	<ul id="navigation">
 
+	<li><a style="text-decoration: none;" href="../../Controller/fluxTest.php">Résultats au fil de l'eau</a></li>
+		<span id="barre">I</span>
 		<li><a style="text-decoration: none;" href="../../Controller/search_member_c.php/?search=Id_Manager">Mes Pilotes</a></li>
 		<span id="barre">I</span>
 		<li><a style="text-decoration: none;" href="../../Controller/search_member_c.php/?search=AllManager">Managers</a></li>
