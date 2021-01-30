37033087cc1fc1fc2baf6e5b750ab7f348a9a7c2
Crakleurs <eti.faviere@gmail.com>
2021-01-30 16:49:04 +0100

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '094e1f5daeb08681b5585338fd0009abea021dec',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '094e1f5daeb08681b5585338fd0009abea021dec',
+    ),
+    'heroku/heroku-buildpack-php' => 
+    array (
+      'pretty_version' => 'v188',
+      'version' => '188.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9e577b876c529a24c0ba18d2df063d1a06c55af7',
+    ),
+  ),
+);
