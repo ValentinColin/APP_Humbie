1eb16172cc3164158375504b134b30d6465faddd
Valentin <valentin.colin78@gmail.com>
2021-01-31 16:15:22 +0100

    clear page home

--- en_EN/php/View/Manager/home.php
+++ en_EN/php/View/Manager/home.php
@@ -6,7 +6,6 @@ verif_access('MANAGER');
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 	<meta charset="UTF-8">
 	<title>Humbie</title>
@@ -14,38 +13,22 @@ verif_access('MANAGER');
 	<!-- Pour fixer le footer en bas de la page -->
 	<link rel="stylesheet" type="text/css" href="../../../../css/config.css">
 	<link rel="stylesheet" type="text/css" href="../../../../css/header.css">
+	<link rel="stylesheet" type="text/css" href="../../../../css/nav.css">
 	<link rel="stylesheet" type="text/css" href="../../../../css/footer.css">
 	<link rel="stylesheet" type="text/css" href="../../../../css/home.css">
-	<link rel="stylesheet" type="text/css" href="../../../../css/nav.css">
-	<!-- <link rel="icon" href="../../../Images/logo_Humbie.png"> Ne fonctionne pas -->
-	<link rel="script" type="text/css" href="../../../../js/drawGraph.js">
 </head>
 
 <body>
 	<?php require('header.php'); ?>
-	<?php require('nav.php') ?>
-
+	<?php require('nav.php'); ?>
 	<img src="../../../../Images/Remplissage_gauche.png" id="remplissage-gauche">
-	<div>
-		<div id="box-content" class="my-block">
-			<main onclick="draw()">
-				<!-- Corps -->
-				<?= '<p>Bonjour ' . $_SESSION['prenom'] . ' ' . $_SESSION['nom'] . ' [' . $_SESSION['access'] . '] !</p>' .
-					'<p>You are currently on the main page.</p>';
-				?>
-				<label>
-					<h2>A graphic</h2>
-					<svg id="graphExemple1" width="900" height="450"></svg>
-				</label>
-			</main>
-		</div>
-	</div>
+
+	<main>
+		<p>Welcome to Humbie's website <?= $_SESSION['prenom'].' '.$_SESSION['nom'] ?> !</p>
+	</main>
+
 	<!-- <aside id="blank"></aside> -->
 	<!-- Permet de bloquer le footer en bas de l'écran -->
 	<?php require('footer.php'); ?>
 </body>
-
-<script src="../../../js/jsGraphDisplay.1.0.js"></script> <!-- Script source permettant de dessiner des graphiques -->
-<script src="../../../js/drawGraph.js"></script> <!-- Script qui détermine quoi afficher comme graphique (et avec quelles données) -->
-
 </html>
