bd595d0cdeae640053d1129bb0f4d4c66f795d36
Valentin <valentin.colin78@gmail.com>
2021-02-01 20:22:44 +0100

    restore

--- fr_FR/php/View/divSearch.php
+++ fr_FR/php/View/divSearch.php
@@ -1,7 +1,7 @@
 <form id='affinerRecherche' method='post' action='../../Controller/search_member_c.php/?search=AllMember'>
   <span span class="titreClassemement"> Classer par rôle: </span> <br>
-  oui <input name='triRole' type='radio' value='oui' <?php if ($_SESSION['triRole'] == "oui") : ?> title='tri déjà effectif' checked <?php endif ?>>
-  non <input name='triRole' type='radio' value="non" <?php if ($_SESSION['triRole'] != "oui") : ?> title='tri déjà effectif' <?php endif ?>> <br>
+  oui <input name='triRole' type='radio' value='oui' <?php if ($_SESSION['triRole'] == "oui") : ?> checked='checked' title='tri déjà effectif' <?php endif ?>>
+  non <input name='triRole' type='radio' value="non" <?php if ($_SESSION['triRole'] != "oui") : ?> checked='checked' title='tri déjà effectif' <?php endif ?>> <br>
   <span class="titreClassemement"> Classer les noms par ordre alphabétique : </span> <br>
   <span> oui <span>
       <input name='ordre' type="radio" value="croissant" <?php if ($_SESSION['triOrdre'] != "decroissant") : ?> checked='checked' title='tri déjà effectif' <?php endif ?>>
