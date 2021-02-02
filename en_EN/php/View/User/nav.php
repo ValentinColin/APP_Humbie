73e8588a4253f5badbefaac4554445369cbb47a0
Valentin <valentin.colin78@gmail.com>
2021-02-02 13:18:23 +0100

    correction

--- en_EN/php/View/User/nav.php
+++ en_EN/php/View/User/nav.php
@@ -1,12 +1,11 @@
 <nav id='mainNav'>
 	<ul id="navigation">
-
 		<span id="barre">I</span>
 		<li><a style="text-decoration: none;" href="../../Controller/results.php">My test results</a></li>
 		<span id="barre">I</span>
-		<li><a style="text-decoration: none;" href="../../Controller/ticket.php">Tickets</a></li>
-		<span id="barre">I</span>
 		<li><a style="text-decoration: none;" href="../../Controller/search_member_c.php/?search=AllMember">Directory</a></li>
+		<span id="barre">I</span>
+		<li><a style="text-decoration: none;" href="../../Controller/ticket.php">Tickets</a></li>
 		<span id="barre">I</span>
 		<li><a style="text-decoration: none;" href="faq.php">FAQ</a></li>
 		<span id="barre">I</span>
--- fr_FR/php/View/User/nav.php
+++ fr_FR/php/View/User/nav.php
@@ -1,11 +1,8 @@
 <nav id='mainNav'>
 	<ul id="navigation">
-
 		<span id="barre">I</span>
 		<li><a style="text-decoration: none;" href="../../Controller/results.php">Mes Résultats de Tests</a></li>
-		<span id="barre">I</span><!-- 
-		<li><a style="text-decoration: none;" href="empty">Mes Statistiques</a></li>
-		<span id="barre">I</span> -->
+		<span id="barre">I</span>
 		<li><a style="text-decoration: none;" href="../../Controller/search_member_c.php/?search=AllMember">Annuaire</a></li>
 		<span id="barre">I</span>
 		<li><a style="text-decoration: none;" href="../../Controller/ticket.php">Tickets</a></li>
